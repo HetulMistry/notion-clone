91af9a3a0bbd48a4b549993921d76a307cca6700
Anonymous.exe <manishcmdgm812@gmail.com>
2023-11-17 19:35:57 +0530

    Text editing and error page added ,new note creation method changed, modified gitignore to exclude folder customizations.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,18 +2,23 @@
 
 import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
-import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
 import { useMutation } from "convex/react";
-import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
 const DocumentsPage = () => {
+  const router = useRouter();
   const { user } = useUser();
   const create = useMutation(api.documents.create);
 
   const onCreate = () => {
-    const promise = create({ title: "Untitled" });
+    const promise = create({ title: "Untitled" }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
 
     toast.promise(promise, {
       loading: "Creating a new note...",
@@ -28,18 +33,18 @@ const DocumentsPage = () => {
         src="/empty.png"
         height="300"
         width="300"
-        alt="empty"
+        alt="Empty"
         className="dark:hidden"
       />
       <Image
         src="/empty-dark.png"
         height="300"
         width="300"
-        alt="empty"
+        alt="Empty"
         className="hidden dark:block"
       />
       <h2 className="text-lg font-medium">
-        Welcome to {user?.firstName}&apos;s Notion
+        Welcome to {user?.firstName}&apos;s Jotion
       </h2>
       <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
